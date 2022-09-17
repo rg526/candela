45d0d431b24976fca7b599ab64c2263e2e3cfc0d
hyabc <yizetang@hotmail.com>
2022-09-17 17:41:48 -0400

    Add User model/table

---
+++ cdmodel/user.go
@@ -0,0 +1,9 @@
+package cdmodel
+
+type User struct {
+	UID			string
+	Name		string
+	GivenName	string
+	FamilyName	string
+	Email		string
+}
---
+++ cdmodel/user.sql
@@ -0,0 +1,13 @@
+USE candela;
+CREATE TABLE user (
+	uid					VARCHAR(128)	NOT NULL PRIMARY KEY,
+	name				TEXT			NOT NULL,
+	givenName			TEXT			NOT NULL,
+	familyName			TEXT			NOT NULL,
+	Email				TEXT			NOT NULL
+);
+
+CREATE TABLE token (
+	token				VARCHAR(128)		NOT NULL PRIMARY KEY,
+	uid					VARCHAR(128)	NOT NULL
+);
