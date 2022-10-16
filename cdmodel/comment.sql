1d8bad4d052d089eaf84fcf6f9cb41a8957c3a3f
hyabc <yizetang@hotmail.com>
2022-10-16 11:39:16 -0400

    Add comment table schema

---
+++ cdmodel/comment.sql
@@ -0,0 +1,24 @@
+USE candela;
+
+CREATE TABLE comment (
+	commentID			INT				NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	cid					VARCHAR(128)	NOT NULL,
+	uid					VARCHAR(128)	NOT NULL,
+	content				MEDIUMTEXT		NOT NULL,
+	time				TEXT			NOT NULL
+);
+
+CREATE TABLE comment_reply (
+	replyID				INT				NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	commentID			INT				NOT NULL,
+	uid					VARCHAR(128)	NOT NULL,
+	content				MEDIUMTEXT		NOT NULL,
+	time				TEXT			NOT NULL
+);
+
+CREATE TABLE comment_response (
+	responseID			INT				NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	commentID			INT				NOT NULL,
+	uid					VARCHAR(128)	NOT NULL,
+	time				TEXT			NOT NULL
+);
