a15116ff468fcb5bc6e9826a2af9eb5ff1e24f7a
hyabc <yizetang@hotmail.com>
2022-10-16 21:40:14 -0400

    Support comment reply

--- cdengine/course.go
+++ cdengine/course.go
@@ -4,6 +4,7 @@ import (
 	"time"
 	"strconv"
 	"net/http"
+	"database/sql"
 	"github.com/gin-gonic/gin"
 
 	"candela/cdmodel"
@@ -88,7 +89,17 @@ func GetCourseComment(ctx *gin.Context, ectx *Context) {
 	cid := ctx.Param("cid")
 
 	// Query DB
-	stmtComment, err := ectx.DB.Prepare("SELECT commentID, content, time, anonymous, comment.uid, user.name FROM comment INNER JOIN user ON comment.uid = user.uid WHERE cid = ?")
+	stmtComment, err := ectx.DB.Prepare(`SELECT
+		comment.commentID, comment.content, comment.time, comment.anonymous, comment.uid, user_comment.name,
+		reply.replyID, reply.content, reply.time, reply.anonymous, reply.uid, user_reply.name
+		FROM comment
+		LEFT JOIN comment_reply AS reply
+		ON comment.commentID = reply.commentID
+		INNER JOIN user AS user_comment
+		ON comment.uid = user_comment.uid
+		LEFT JOIN user AS user_reply
+		ON reply.uid = user_reply.uid
+		WHERE cid = ?`)
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, gin.H{
 			"Status": "ERROR",
@@ -107,36 +118,87 @@ func GetCourseComment(ctx *gin.Context, ectx *Context) {
 	// Append comments to array
 	var commentArr []cdmodel.Comment
 	for rows.Next() {
-		var comment cdmodel.Comment
-		var commentTime, commentUID string
-		var commentAnonymous int
-		err = rows.Scan(&comment.CommentID, &comment.Content, &commentTime,
-			&commentAnonymous, &commentUID, &comment.Author)
+		var commentQuery struct {
+			CommentID		int
+			Content			string
+			Time			string
+			Anonymous		int
+			UID				string
+			Author			string
+		}
+		var replyQuery struct {
+			ReplyID			sql.NullInt32
+			Content			sql.NullString
+			Time			sql.NullString
+			Anonymous		sql.NullInt32
+			UID				sql.NullString
+			Author			sql.NullString
+		}
+
+		err = rows.Scan(&commentQuery.CommentID, &commentQuery.Content, &commentQuery.Time,
+			&commentQuery.Anonymous, &commentQuery.UID, &commentQuery.Author,
+			&replyQuery.ReplyID, &replyQuery.Content, &replyQuery.Time, &replyQuery.Anonymous, &replyQuery.UID, &replyQuery.Author)
 		if err != nil {
 			ctx.JSON(http.StatusInternalServerError, gin.H{
 				"Status": "ERROR",
 				"Error": "Error: " + err.Error()})
 			return
 		}
-		// Protect UID/Anonymous
-		comment.Self = commentUID == user.UID
-		if (commentAnonymous == 1) {
-			comment.Author = "Anonymous"
-			if (comment.Self) {
-				comment.Author = "Anonymous (self)"
+
+		// Insert comment
+		if (len(commentArr) == 0 ||
+			commentArr[len(commentArr) - 1].CommentID != commentQuery.CommentID) {
+			var comment cdmodel.Comment
+			comment.CommentID = commentQuery.CommentID
+			comment.Content = commentQuery.Content
+			comment.Self = commentQuery.UID == user.UID
+
+			// Convert time from unix ts (string) into readable string
+			commentTimeUnix, err := strconv.ParseInt(commentQuery.Time, 10, 64)
+			if err != nil {
+				comment.Time = err.Error()
+			} else {
+				comment.Time = time.Unix(commentTimeUnix, 0).String()
 			}
-		}
-		comment.Time = commentTime // TODO: convert to readable time
 
-		// Convert time from unix ts (string) into readable string
-		commentTimeUnix, err := strconv.ParseInt(commentTime, 10, 64)
-		if err != nil {
-			comment.Time = err.Error()
-		} else {
-			comment.Time = time.Unix(commentTimeUnix, 0).String()
+			// Protect Anonymous
+			if (commentQuery.Anonymous == 1) {
+				comment.Author = "Anonymous"
+			} else {
+				comment.Author = commentQuery.Author
+			}
+
+			// Insert to commentArr
+			commentArr = append(commentArr, comment)
 		}
 
-		commentArr = append(commentArr, comment)
+		// Insert reply
+		if (replyQuery.ReplyID.Valid) {
+			var reply cdmodel.CommentReply
+			reply.ReplyID = int(replyQuery.ReplyID.Int32)
+			reply.Content = replyQuery.Content.String
+			reply.Self = replyQuery.UID.String == user.UID
+
+			// Convert time
+			replyTimeUnix, err := strconv.ParseInt(replyQuery.Time.String, 10, 64)
+			if err != nil {
+				reply.Time = err.Error()
+			} else {
+				reply.Time = time.Unix(replyTimeUnix, 0).String()
+			}
+
+			// Protect Anonymous
+			if (replyQuery.Anonymous.Int32 == 1) {
+				reply.Author = "Anonymous"
+			} else {
+				reply.Author = replyQuery.Author.String
+			}
+
+			// Insert into last comment
+			replies := &commentArr[len(commentArr) - 1].Replies
+			*replies = append(*replies, reply)
+		}
+
 	}
 
 	// Return result
