8e5bb7f9534c2e7b372b3eb771bd77e1b46b571a
hyabc <yizetang@hotmail.com>
2022-10-18 10:05:25 -0400

    Split fce from course table

--- cdmodel/base.sql
+++ cdmodel/base.sql
@@ -1,18 +1,22 @@
 USE candela;
 CREATE TABLE course (
 	cid 				VARCHAR(128)	NOT NULL PRIMARY KEY,
-	name				TEXT		NOT NULL,
-	description			MEDIUMTEXT	NOT NULL,
-	dept				TEXT		NOT NULL,
-	units				DECIMAL		NOT NULL,
-	prof				TEXT		NOT NULL,
-	prereq				TEXT		NOT NULL,
-	coreq				TEXT		NOT NULL,
-	FCEHours			DECIMAL		NOT NULL,
-	FCETeachingRate		DECIMAL		NOT NULL,
-	FCECourseRate		DECIMAL		NOT NULL,
-	FCELevel			TEXT		NOT NULL,
-	FCEStudentCount		INTEGER		NOT NULL
+	name				TEXT			NOT NULL,
+	description			MEDIUMTEXT		NOT NULL,
+	dept				TEXT			NOT NULL,
+	units				DECIMAL			NOT NULL,
+	prof				TEXT			NOT NULL,
+	prereq				TEXT			NOT NULL,
+	coreq				TEXT			NOT NULL
+);
+
+CREATE TABLE fce (
+	cid					VARCHAR(128)	NOT NULL PRIMARY KEY,
+	hours				DECIMAL			NOT NULL,
+	teachingRate		DECIMAL			NOT NULL,
+	courseRate			DECIMAL			NOT NULL,
+	level				TEXT			NOT NULL,
+	studentCount		INTEGER			NOT NULL
 );
 
 CREATE TABLE professor (
---
+++ cdsource/rmp_import.py
@@ -0,0 +1,24 @@
+import json
+import mysql.connector
+import config
+
+
+with open("rmp_data.json", "r") as f:
+	rmp_data = json.load(f)
+
+
+conn = mysql.connector.connect(
+	host = config.DB_HOST,
+	user = config.DB_USER,
+	password = config.DB_PWD,
+	database = config.DB_NAME,
+	autocommit = True
+)
+cursor = conn.cursor()
+
+
+# professor table
+for name, prof in rmp_data.items():
+	sql = "INSERT INTO professor (name, RMPRatingClass, RMPRatingOverall) VALUES (%s, %s, %s)"
+	val = (name, prof["RMPRatingClass"], prof["RMPRatingOverall"])
+	cursor.execute(sql, val)
