073d703ec57d029ad6a43cfd34c33b3132ea0127
hyabc <yizetang@hotmail.com>
2022-10-20 19:49:05 -0400

    Search in tag field

--- cdengine/search.go
+++ cdengine/search.go
@@ -33,15 +33,18 @@ func GetSearch(ctx *gin.Context, ectx *Context) {
 			ON course.cid = fce.cid
 			LEFT JOIN page
 			ON course.cid = page.cid
+			LEFT JOIN tag
+			ON course.cid = tag.cid
 			LEFT JOIN prof
 			ON course.cid = prof.cid
 			WHERE
 				(LOCATE(?, course.cid) > 0 OR LOCATE(?, course.name) > 0 OR
 					LOCATE(?, course.description) > 0 OR LOCATE(?, course.dept) > 0 OR
 					LOCATE(?, page.content) > 0 OR
+					LOCATE(?, tag.content) > 0 OR
 					LOCATE(?, prof.name) > 0) `
 	var args []interface{}
-	for i := 0;i < 6;i++ {
+	for i := 0;i < 7;i++ {
 		args = append(args, query)
 	}
 	if ctx.Query("is_advanced") == "true" {
