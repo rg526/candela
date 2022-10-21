188a3fe03165b6e871150c4c9fb91bca19653feb
hyabc <yizetang@hotmail.com>
2022-10-20 20:12:12 -0400

    Add UID field to DB

--- cdmodel/tag.sql
+++ cdmodel/tag.sql
@@ -4,5 +4,6 @@ CREATE TABLE tag (
 	tagID				INT				NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	cid					VARCHAR(128)	NOT NULL,
 	content				TEXT			NOT NULL,
-	priority			INT				NOT NULL DEFAULT 0
+	priority			INT				NOT NULL DEFAULT 0,
+	uid                 VARCHAR(128)    NOT NULL
 );
